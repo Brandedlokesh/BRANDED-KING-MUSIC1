bb9fce730faeadfd31f40d4b7dc4d6847d595840
BRANDED KING <143484617+Brandedlokesh@users.noreply.github.com>
2023-09-16 17:13:40 +0530

    Update inline.py

--- FallenMusic/Helpers/inline.py
+++ FallenMusic/Helpers/inline.py
@@ -29,109 +29,178 @@ close_key = InlineKeyboardMarkup(
     [[InlineKeyboardButton(text="✯ ᴄʟᴏsᴇ ✯", callback_data="close")]]
 )
 
+  def track_markup(_, videoid, user_id, channel, fplay):
+    buttons = [
+        [
+             InlineKeyboardMarkup(
+                text=_["P_B_1"],
+                callback_data=f"MusicStream {videoid}|{user_id}|a|{channel}|{fplay}",
+            ),
+             InlineKeyboardMarkup(
+                text=_["P_B_2"],
+                callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="🥀 ᴏᴡɴᴇʀ 🥀", url="https://t.me/Venom_bolti_public",
+            ),
+            InlineKeyboardButton(
+                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url="https://t.me/Venom_Chatting",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data=f"forceclose {videoid}|{user_id}",
+            )
+        ],
+    ]
+    return buttons
+
+
 def stream_markup_timer(_, chat_id, played, dur):
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "⚡ѕтαяє∂ ρℓαყเɳɠ⚡"
+        bar = "◉—————————"
     elif 10 < umm < 20:
-        bar = " 💥BRANDED_WORLD💥 "
+        bar = "—◉————————"
     elif 20 <= umm < 30:
-        bar = " 🔥BRANDRD_BOT🔥 "
+        bar = "——◉———————"
     elif 30 <= umm < 40:
-        bar = " 🥀BRANDED_KHUSHI🥀 "
+        bar = "———◉——————"
     elif 40 <= umm < 50:
-        bar = " 💥BRANDED_WORLD💥 "
+        bar = "————◉—————"
     elif 50 <= umm < 60:
-        bar = " 🔥BRANDRD_BOT🔥 "
+        bar = "—————◉————"
     elif 60 <= umm < 70:
-        bar = " 🥀BRANDED_KHUSHI🥀 "
+        bar = "——————◉———"
     elif 70 <= umm < 80:
-        bar = " 💥BRANDED_WORLD💥 "
+        bar = "———————◉——"
     elif 80 <= umm < 95:
-        bar = " 🔥BRANDRD_BOT🔥 "   
+        bar = "————————◉—"
     else:
-        bar = " 🍷ѕσ ¢ιтє ѕσηg🍷 "
-
-buttons = InlineKeyboardMarkup(
-    [
+        bar = "—————————◉"
+    buttons = [
         [
-            InlineKeyboardButton(text="🕹️Rᴇsᴜᴍᴇ🕹️", callback_data="resume_cb"),
-            InlineKeyboardButton(text="🕹️Pᴀᴜsᴇ🕹️", callback_data="pause_cb"),
-        ], 
+            InlineKeyboardMarkup(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardMarkup(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardMarkup(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardMarkup(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
         [
-            InlineKeyboardButton(text="🕹️Sᴋɪᴘ🕹️", callback_data="skip_cb"),
-            InlineKeyboardButton(text="🕹️Eɴᴅ🕹️", callback_data="end_cb"), 
-        ], 
+            InlineKeyboardMarkup(
+                text=f"{played} {bar} {dur}",
+                callback_data="GetTimer",
+            )
+            ],
+        [
+            InlineKeyboardButton(
+                text="🥀 ᴏᴡɴᴇʀ 🥀", url="https://t.me/Venom_bolti_public",
+            ),
+            InlineKeyboardButton(
+                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url="https://t.me/Venom_Chatting",
+            )
+        ],
+        [IInlineKeyboardMarkup(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
-)
+    return buttons
 
 
-pm_buttons = [
-    [
-        InlineKeyboardButton(
-            text="🌹 ᴀᴅᴅ ʏᴏᴜʀ ɢʀᴘ ʀᴀᴅʜᴇ ʀᴀᴅʜᴇ 🥀 ",
-            url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-        )
-    ],
-    [InlineKeyboardButton(text="🌹ʜᴇʟᴩ & ᴄᴏᴍᴍᴀɴᴅs🦋", callback_data="fallen_help")],
-    [
-        InlineKeyboardButton(text="🌹 ᴄʜᴀɴɴᴇʟ🦋 ", url=config.SUPPORT_CHANNEL),
-        InlineKeyboardButton(text=" 🌹 sᴜᴩᴩᴏʀᴛ 🦋", url=config.SUPPORT_CHAT),
-    ],
-    [
-        InlineKeyboardButton(
-            text="🌹 sᴏᴜʀᴄᴇ 🦋", url="https://te.legra.ph/file/3d0d7d23d3a7fb86b442e.jpg"
-        ),
-        InlineKeyboardButton(text="🌹 ʙʀᴀɴᴅᴇᴅ🦋 ", user_id=config.OWNER_ID),
-    ],
-]
+def stream_markup(_, chat_id):
+    buttons = [
+        [
+            InlineKeyboardMarkup(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardMarkup(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardMarkup(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardMarkup(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardMarkup(text="▢", callback_data=f"ADMIN Stop|{chat_id}")
+            ],
+        [
+            InlineKeyboardButton(
+                text="🥀 ᴏᴡɴᴇʀ 🥀", url="https://t.me/Venom_bolti_public",
+            ),
+            InlineKeyboardButton(
+                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url="https://t.me/Venom_Chatting",
+            )
+        ],
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+    ]
+    return buttons
 
 
-gp_buttons = [
-    [
-        InlineKeyboardButton(
-            text="🌹 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🥀",
-            url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-        )
-    ],
-    [
-        InlineKeyboardButton(text="🌹 ᴄʜᴀɴɴᴇʟ🦋 ", url=config.SUPPORT_CHANNEL),
-        InlineKeyboardButton(text=" 🌹sᴜᴩᴩᴏʀᴛ🦋 ", url=config.SUPPORT_CHAT),
-    ],
-    [
-        InlineKeyboardButton(
-            text="🌹  sᴏᴜʀᴄᴇ🦋 ", url="https://te.legra.ph/file/3d0d7d23d3a7fb86b442e.jpg"
-        ),
-        InlineKeyboardButton(text="🌹 ʙʀᴀɴᴅᴇᴅ🦋 ", user_id=config.OWNER_ID),
-    ],
-]
+def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
+    buttons = [
+        [
+            InlineKeyboardMarkup(
+                text=_["P_B_1"],
+                callback_data=f"AyushPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+            ),
+            InlineKeyboardMarkup(
+                text=_["P_B_2"],
+                callback_data=f"AyushPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+            ),
+        ],
+        [
+            InlineKeyboardMarkup(
+                text=_["CLOSE_BUTTON"],
+                callback_data=f"forceclose {videoid}|{user_id}",
+            ),
+        ],
+    ]
+    return buttons
 
 
-helpmenu = [
-    [
-        InlineKeyboardButton(
-            text="ᴇᴠᴇʀʏᴏɴᴇ",
-            callback_data="fallen_cb help",
-        )
-    ],
-    [
-        InlineKeyboardButton(text="🌹sᴜᴅᴏ🦋", callback_data="fallen_cb sudo"),
-        InlineKeyboardButton(text="🌹ᴏᴡɴᴇʀ🦋", callback_data="fallen_cb owner"),
-    ],
-    [
-        InlineKeyboardButton(text="🌹ʙᴀᴄᴋ🦋", callback_data="fallen_home"),
-        InlineKeyboardButton(text="🌹ᴄʟᴏsᴇ🦋", callback_data="close"),
-    ],
-]
+def livestream_markup(_, videoid, user_id, mode, channel, fplay):
+    buttons = [
+        [
+            InlineKeyboardMarkup(
+                text=_["P_B_3"],
+                callback_data=f"LiveStream {videoid}|{user_id}|{mode}|{channel}|{fplay}",
+            ),
+        ],
+        [
+            InlineKeyboardMarkup(
+                text=_["CLOSE_BUTTON"],
+                callback_data=f"forceclose {videoid}|{user_id}",
+            ),
+        ],
+    ]
+    return buttons
 
 
-help_back = [
-    [InlineKeyboardButton(text=" 🌹sᴜᴩᴩᴏʀᴛ🦋 ", url=config.SUPPORT_CHAT)],
-    [
-        InlineKeyboardButton(text="🌹ʙᴀᴄᴋ🦋", callback_data="fallen_help"),
-        InlineKeyboardButton(text="🌹ᴄʟᴏsᴇ🦋", callback_data="close"),
+def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
+    query = f"{query[:20]}"
+    buttons = [
+        [
+            InlineKeyboardMarkup(
+                text=_["P_B_1"],
+                callback_data=f"MusicStream {videoid}|{user_id}|a|{channel}|{fplay}",
+            ),
+            InlineKeyboardMarkup(
+                text=_["P_B_2"],
+                callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
+            ),
+        ],
+        [
+            InlineKeyboardMarkup(
+                text="◁",
+                callback_data=f"slider B|{query_type}|{query}|{user_id}|{channel}|{fplay}",
+            ),
+            InlineKeyboardMarkup(
+                text=_["CLOSE_BUTTON"],
+                callback_data=f"forceclose {query}|{user_id}",
+            ),
+            InlineKeyboardMarkup(
+                text="▷",
+                callback_data=f"slider F|{query_type}|{query}|{user_id}|{channel}|{fplay}",
+            ),
+        ],
+    ]
+    return buttons
     ],
 ] 

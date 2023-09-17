ef1c45ace8477342423e12a66430a41b67ff799d
BRANDED KING <143484617+Brandedlokesh@users.noreply.github.com>
2023-09-17 12:33:33 +0530

    Update inline.py

--- FallenMusic/Helpers/inline.py
+++ FallenMusic/Helpers/inline.py
@@ -54,7 +54,7 @@ def stream_markup_timer(_, chat_id, played, dur):
         bar = "————————◉—"
     else:
         bar = "—————————◉"
-    buttons = [
+    
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             lineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
@@ -78,11 +78,11 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
-    return buttons
+    
 
 
 def stream_markup(_, chat_id):
-    buttons = [
+    
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
@@ -100,11 +100,11 @@ def stream_markup(_, chat_id):
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
-    return buttons
+    
 
 
 def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
-    buttons = [
+    
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
@@ -122,11 +122,11 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
             ),
         ],
     ]
-    return buttons
+    
 
 
 def livestream_markup(_, videoid, user_id, mode, channel, fplay):
-    buttons = [
+    
         [
             InlineKeyboardButton(
                 text=_["P_B_3"],
@@ -140,12 +140,12 @@ def livestream_markup(_, videoid, user_id, mode, channel, fplay):
             ),
         ],
     ]
-    return buttons
+     
 
 
 def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
     query = f"{query[:20]}"
-    buttons = [
+     
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
